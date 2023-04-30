cb795ac796d060d0f1328f3a787461ed915ce980
Aniv Surana <114275664+anivsurana@users.noreply.github.com>
2023-04-30 02:22:53 +0000

    Removing excess code

--- mfs.c
+++ mfs.c
@@ -1,30 +1,4 @@
-/*
-  Name: Aniv Surana
-  ID: 1001912967
-*/
 
-// The MIT License (MIT)
-// 
-// Copyright (c) 2016 Trevor Bakker 
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
 
 #define _GNU_SOURCE
 
@@ -167,23 +141,6 @@ int main()
     char *head_ptr = working_string;
     int nf_count = 0;
 
-    // Check if the first character in the command (in case of !)
-    if(working_string[0] == '!')
-    {
-      int command_number = atoi(&working_string[1]);
-
-      // checking if command_number corresponds to a valid command in history
-      if(command_number < hist_count && command_number > -1)
-      {
-        strcpy(working_string,history[command_number]);
-      }
-      else
-      {
-        // not found in history and increments the counter variable
-        printf("Command not in history.\n");
-        nf_count++; 
-      }
-    }
     if (working_string[0] == '\n' || working_string[0] == ' ' || working_string[0] == '\t') 
     {
       continue;
@@ -219,38 +176,6 @@ int main()
     //   printf("token[%d] = %s\n", token_index, token[token_index] );  
     // }
 
-    /*
-
-
-        My Shell's Functionality:
-    
-    
-    */
-
-    if(token[0] != NULL)
-    {
-      // duplicating the input string
-      char *line = strdup(command_string);
-
-      history[hist_index] = line;
-
-      // incrementing each time an element is added to history
-      hist_count++;
-      hist_index++;
-
-
-
-      // making sure number of arguments in history doesn't exceed the max
-      if(hist_count > MAX_HIST_ARGUMENTS)
-      {
-        hist_count = 0;
-      }
-      // resetting history index
-      if(hist_index > MAX_HIST_ARGUMENTS-1)
-      {
-
-        hist_index = 0;
-      }
 
       // exit the shell if user enters quit or exit
       if(strcmp(token[0],"quit") == 0 || strcmp(token[0],"exit") == 0)
@@ -258,98 +183,7 @@ int main()
         exit(0);
       }
 
-      pid_t pid = fork();
-
-      // storing pid of child process in list
-      if(strcmp(token[0], "cd") == 0 || strcmp(token[0],"history") == 0)
-      {
-        pid_list[pid_index] = -1;
-      }
-      else
-      {
-        pid_list[pid_index] = pid;
-      }
-      pid_index++;
-      pid_count++;
-
-
-      // making sure number of pids doesn't exceed the max
-      if(pid_index > MAX_PIDS - 1)
-      {
-        pid_index = 0;
-      }
-      if(pid_count > MAX_PIDS)
-      {
-        pid_count = MAX_PIDS;
-      }
-
-      // check for the parent process
-      // return value for the parent is a non-zero value
-      if(pid != 0)
-      {
-        if(strcmp(token[0],"cd") == 0)
-        {
-          // token[1] is the directory to be changed to
-          chdir(token[1]);
-        }
-      }
-      
-      // check for the child process
-      // child process will receive the value 0 when forked
-      if(pid == 0)
-      {
-        int valid = execvp(token[0], &token[0]);
-        if(valid == -1)
-        {
-          // output if the command cannot be executed
-          if(strcmp(token[0],"history") != 0 && strcmp(token[0],"cd") != 0 && nf_count < 1)
-          {
-            printf("%s: Command not found.\n",token[0]);
-          }
-          break;
-        }
-      }
-
-      else
-      {
-        // waiting for the child process to complete
-        int status;
-        wait(&status);
-      }
-    }
     
-    // displaying the history
-    if(strcmp(token[0],"history") == 0)
-      {
-        printf("\n");
-        printf("HISTORY: \n");
-
-        // flag variable to check if -p is entered by the user
-        int print_pids = 0;
-        if(token[1] != NULL && strcmp(token[1],"-p") == 0)
-        {
-          print_pids = 1;
-        }
-        // printing based on number of commands in history
-        for(int i =0; i < hist_count; i++)
-        {
-          // eliminating the newline character to make it easier to print
-          char *commandtoprint = strtok(history[i],"\n");
-
-          if(print_pids == 1 && pid_list[i] != 0)
-          {
-            // printing pid in [] before the command
-            printf("%2d. [%5d] %s",i,pid_list[i],commandtoprint);
-          }
-          else
-          {
-            printf("%2d. %s",i,history[i]);
-          }
-          printf("\n");
-        }
-        printf("\n");
-      }
-
    
 
     // Cleanup allocated memory
